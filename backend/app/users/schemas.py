6b71fe758eac47bf84623f937e5b087aaabc7f51
Lev Khalyapin <lev.halyapin@yandex.ru>
2023-03-06 15:28:57 +0300

    Добавлены схемы для токена

--- backend/app/users/schemas.py
+++ backend/app/users/schemas.py
@@ -81,3 +81,12 @@ class Player(PlayerBase):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: EmailStr | None = None
