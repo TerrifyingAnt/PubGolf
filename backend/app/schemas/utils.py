331123e7fd0e69eaf8bc4bec543a9a30d5d3db44
Лев Халяпин <lev.halyapin@yandex.ru>
2023-03-16 10:56:23 +0300

    Переехали на Django

--- backend/app/schemas/utils.py
+++
@@ -1,8 +0,0 @@
-username_regex = r'[A-Za-z0-9@#$%^&+=]'
-password_regex = r'[A-Za-z0-9@#$%^&+=]{8,}'
-
-
-def validate_re_password(value: str, values: dict) -> str:
-    if 'password' in values and value != values['password']:
-        raise ValueError('Пароли не совпадают')
-    return value
