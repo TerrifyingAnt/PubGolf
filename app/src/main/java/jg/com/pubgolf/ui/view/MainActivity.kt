aec88f6d2ccbe8ff659b13b335587bacbc9605da
Ant <TerrifyingAnt@yandex.ru>
2023-05-11 02:07:43 +0300

    Добавлена навигация на главный экран

--- app/src/main/java/jg/com/pubgolf/ui/view/MainActivity.kt
+++ app/src/main/java/jg/com/pubgolf/ui/view/MainActivity.kt
@@ -1,15 +1,37 @@
 package jg.com.pubgolf.ui.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import jg.com.pubgolf.ui.theme.PubGolfTheme
+import jg.com.pubgolf.ui.view.navigation.BottomMenuItem
+import jg.com.pubgolf.ui.view.navigation.MainNavigation
+
+import java.util.*
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,9 +41,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    // TODO
+                    MainNavigation()
                 }
             }
         }
     }
 }
+
+
+
+
