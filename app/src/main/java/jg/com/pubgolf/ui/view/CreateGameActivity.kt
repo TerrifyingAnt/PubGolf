51cf6f2914e46f7437a57cd20059cbbc854de044
Ant <TerrifyingAnt@yandex.ru>
2023-05-11 02:07:19 +0300

    Добавлены экраны переходов по навигации

---
+++ app/src/main/java/jg/com/pubgolf/ui/view/CreateGameActivity.kt
@@ -0,0 +1,30 @@
+package jg.com.pubgolf.ui.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import jg.com.pubgolf.ui.theme.PubGolfTheme
+import jg.com.pubgolf.ui.view.navigation.MainNavigation
+
+class CreateGameActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            PubGolfTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Text("Активити создания игры")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/jg/com/pubgolf/ui/view/navigation/screens/HistoryScreen.kt
@@ -0,0 +1,9 @@
+package jg.com.pubgolf.ui.view.navigation.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen() {
+    Text("Здесь должна быть история всех игр")
+}
